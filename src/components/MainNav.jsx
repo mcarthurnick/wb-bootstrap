d7c54b3b9bd50f4a3a314157ff251542d80ca9ab
Nick McArthur <mcarthurnick@gmail.com>
2023-08-03 14:19:24 -0600

    Updated Navbar using React-Boostrap components

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,21 +1,29 @@
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 export default function MainNav({ brand, leftLinks, rightLinks }) {
+
+
+  
   return (
-    <nav>
-      <a href="/">{brand}</a>
-      <ul>
-        {leftLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {rightLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+      <Navbar className="bg-success navbar-dark">
+        <Container fluid>
+        <Navbar.Brand href="/">{brand}</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            {leftLinks.map(({ url, text }) => (
+              <Nav.Link key={text} href={url}>{text}</Nav.Link>
+            ))}
+          </Nav>
+          <Nav>
+            {rightLinks.map(({ url, text }) => (
+              <Nav.Link key={text} href={url}>{text}</Nav.Link>
+            ))}
+          </Nav>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+ );
 }
